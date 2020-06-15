a482d5087ae7ed2428984a09b6a6fb20f8fe1ae0
LAMurakami <GitHub@LAMurakami.com>
2020-06-14 21:24:53 -0800

    Add aws-nwo-lam1-Ubuntu-CloudInit.txt
    Add aws-nwo-lam1-Ubuntu-CloudInit.txt and cloud-init.pl
    
    Update README.md with notes on cloud-init.pl

--- README.md
+++ README.md
@@ -30,3 +30,16 @@ This would then be picked up by the Daily cron job to backup
  #!/bin/bash
  run-parts --report /mnt/efs/aws-lam1-ubuntu
  [19:34:30 Sunday 06/14/2020] ubuntu@aws
+
+A new instance can be launched from the command line with the following:
+
+ aws ec2 run-instances --count 1 --image-id ami-003634241a8fcdec0 \
+ --instance-type t3.nano --security-group-ids sg-3bda0647 \
+ --associate-public-ip-address --key-name aws-nwo-lam1 --user-data \
+ file:///var/www/aws/aws-nwo-lam1-Ubuntu-CloudInit.txt
+
+The CloudInit includes a call to and cloud-init.pl which updates
+/var/www/aws/html/index.html and /var/www/aws/aws_apache2.conf
+based on some /var/log/cloud-init-output.log values generated
+by /var/www/aws/aws-nwo-lam1-Ubuntu-CloudInit.txt directives.
+
---
+++ cloud-init.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+# $Id: cloud-init.pl,v 1.3 2018/04/18 09:05:00 ubuntu Exp ubuntu $
+
+my $configFile = '/var/log/cloud-init-output.log';
+open (CONFIG, $configFile)                             # Open configuration file
+ or                                                    # or report error and end
+  die "Run time error: $0\nConfiguration file: $configFile NOT opened!\n$!\n\n";
+
+our %cA = loadConfigHash(); # Load Configuration Parameters hash!
+close(CONFIG);
+
+my $updateFile = '/var/www/aws/html/index.html';
+open (CONFIG, $updateFile)                             # Open configuration file
+ or                                                    # or report error and end
+  die "Run time error: $0\nConfiguration file: $updateFile NOT opened!\n$!\n\n";
+
+our %cB = loadConfigHash(); # Load Configuration Parameters hash!
+close(CONFIG);
+
+print "\n
+  instance-type: $cB{'instance-type'}
+              -> $cA{'instance-type'}
+
+public-hostname: $cB{'public-hostname'}
+              -> $cA{'public-hostname'}
+    public-ipv4: $cB{'public-ipv4'}
+              -> $cA{'public-ipv4'}
+
+ local-hostname: $cB{'local-hostname'}
+              -> $cA{'local-hostname'}
+     local-ipv4: $cB{'local-ipv4'}
+              -> $cA{'local-ipv4'}
+";
+
+updateFile($updateFile);
+updateFile('/var/www/aws/aws_apache2.conf');
+# ..:....|....:....|....:....|....:....|....:....|....:....|....:....|....:....|
+sub updateFile { # Update specified file.
+my $fileName = my $textFileString = ''; my $parameterCount = @_;
+if ($parameterCount > 0) {$fileName = shift}
+if(-e $fileName) { # test existance
+     open(READ_FILE, $fileName) or die
+ "Run time error: $0\nFile: $fileName NOT opened for reading!\n$!\n\n";
+
+     while(<READ_FILE>) {$textFileString .= $_ } close(READ_FILE);
+
+$textFileString =~ s/$cB{'public-hostname'}/$cA{'public-hostname'}/g;
+
+$textFileString =~ s/$cB{'public-ipv4'}/$cA{'public-ipv4'}/g;
+
+$textFileString =~ s/$cB{'local-hostname'}/$cA{'local-hostname'}/g;
+
+$textFileString =~ s/$cB{'local-ipv4'}/$cA{'local-ipv4'}/g;
+
+$textFileString =~ s/$cB{'instance-type'}/$cA{'instance-type'}/g;
+
+open (UPDATEFILE, ">$fileName")
+ or
+  die "Run time error: $0\nPage: $fileName NOT opened for update!\n$!\n\n";
+
+print UPDATEFILE $textFileString;
+
+close(UPDATEFILE);
+
+} else {die
+ "Run time error: $0\nFile: $fileName NOT found!\n$!\n\n";}
+
+return}
+
+# ..:....|....:....|....:....|....:....|....:....|....:....|....:....|....:....|
+sub loadConfigHash { my %configParameter = (); # Define a hash!
+
+# modified from Perl Cookbook 8.16 Reading Configuration files
+
+while(<CONFIG>) {          # Load parameters from configuration file into a hash
+    chomp;        # strip newline
+    s/#.*//;      # strip comments
+    s/^\s+//;     # strip leading white space
+    s/\s+$//;     # strip trailing white space
+    next unless length; # skip empty lines
+    last unless not m/Report AWS EC2 user-data for this instance/;
+    my ($var, $value) = split(/\s*:\s+/, $_, 2);
+    $configParameter{$var} = $value;
+}                    # End - Load parameters from configuration file into a hash
+return %configParameter
+}
+# ..:....|....:....|....:....|....:....|....:....|....:....|....:....|....:....|
+
