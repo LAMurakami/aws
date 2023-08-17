19ff947a66422ee7acd273c81ee5d5fe6baec8c0
LAMurakami <GitHub@LAMurakami.com>
2023-08-16 21:38:14 -0800

    Add comments

--- ec2-instance-user-data.bash
+++ ec2-instance-user-data.bash
@@ -1,8 +1,21 @@
 #!/usr/bin/bash
-
+#
+# This program lists the user-data used for initialization.
+# The program runs during the initialization specified in a cloud-config YAML
+#  file or document supplied when the instance is launched.
+# It uses Instance Metadata Service Version 2 (IMDSv2) session-oriented
+#  requests to retrieve the instance-id value from the IMDS
+#  available to an AWS EC2 instance.
+# Updated to v2 of IMDS from v1 which did not need to establish a session
+#  for a request/response to accomodate Amazon Linux 2023 instances
+#  which requires v2 in the default AMIs supplied by Amazon.
+#
+# Establish a 6 hour session which is so much more than we need for this.
+#
 TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
 INSTANCE_ID=$(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
 
+# List cloud-config YAML user-data used for initialization of this instance
+#
 cat /var/lib/cloud/instances/${INSTANCE_ID}/user-data.txt
-
--- ec2-user-data.bash
+++ ec2-user-data.bash
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
+#
+# This program lists name value pairs separated by a colon.
+# The program runs during the initialization specified in a cloud-config YAML
+#  file or document supplied when the instance is launched.
+# It uses Instance Metadata Service Version 2 (IMDSv2) session-oriented
+#  requests to retrieve the ami-id, instance-type, local-hostname, local-ipv4,
+#  public-hostname, public-ipv4, and security-groups values from the IMDS
+#  available to an AWS EC2 instance.
+# Updated to v2 of IMDS from v1 which did not need to establish a session
+#  for a request/response to accomodate Amazon Linux 2023 instances
+#  which requires v2 in the default AMIs supplied by Amazon.
+#
+# Establish a 6 hour session which is so much more than we need for this.
+#
 TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+#
+# List name / value pairs for this instance on separate lines.
+#
 printf "ami-id: %s\n" $(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data//ami-id)
 printf "instance-type: %s\n" $(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data//instance-type)
 printf "local-hostname: %s\n" $(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data//local-hostname)
