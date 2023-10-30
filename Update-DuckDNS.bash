58d2937e29ebaca63f4900330cdb6b599583e005
LAMurakami <GitHub@LAMurakami.com>
2023-10-29 23:32:01 -0800

    Update IPv4 and IPv6 addresses one at a time
    Usage:
    /var/www/aws/Update-DuckDNS.bash [lamurakami|lam1|lam2|larryforalaska]
    
    DuckDNS token file linked to for root from default user.

--- Update-DuckDNS.bash
+++ Update-DuckDNS.bash
@@ -4,6 +4,10 @@ if [ -z $1 ] ; then
   echo "Subdomain for ?.duckdns.org needed!" && exit 1;
 fi
 
+echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
+~/.duckdns)&verbose=true&ip="| curl -k -K -; echo \
+" for $1.duckdns.org IP address update" ; echo
+
 IPv6_Address=$(ip a | grep inet6 \
 | grep 'scope global dynamic' | awk '{print $2;}' \
 | sed 's|/128||' | tr -d '\n')
