27368c1a85c224ed991a4f952d9212e4d996d550
LAMurakami <GitHub@LAMurakami.com>
2023-10-29 22:21:39 -0800

    Add Program to update IPv6 DuckDNS entry
    The IPv6 address must be supplied when updating that DuckDNS entry.
    The IPv4 address need not be supplied to update a DuckDNS entry from
     that server as DuckDNS will use the IP Address of the requestor.

---
+++ Update-DuckDNS.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+  echo "Subdomain for ?.duckdns.org needed!" && exit 1;
+fi
+
+IPv6_Address=$(ip a | grep inet6 \
+| grep 'scope global dynamic' | awk '{print $2;}' \
+| sed 's|/128||' | tr -d '\n')
+
+echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
+~/.duckdns)&verbose=true&ip=&ipv6=${IPv6_Address}"| curl -k -K -; echo \
+" for $1.duckdns.org IP address update" ; echo
+
