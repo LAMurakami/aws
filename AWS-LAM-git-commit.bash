cb1c9d9c73347cc5ae3ab8f19881ec71ae4cfef9
LAMurakami <GitHub@LAMurakami.com>
2023-11-03 00:01:15 -0800

    Update for IPv6 Apache2 implementation - part 4
    aws-nwo-lam1-Ubuntu-CloudInit-ARM.txt # ARM Instance to take over lam1
    
    The Instance to take over lam1 is my main cloud init as it launches a
     full clone of aws.lam1.us with all sites and by default takes over a
     number of the sites including arsc, sites, z and more.
    
    The User data is limited to 16,384 bytes when launching an instance.
    The lam1-Ubuntu-ARM cloud init exceeded this when it grew to 17,719 bytes
     with the addition of the IPv6 changes which included creating
     and commiting 13 branches named with the KEYNAME for the instance.
    
    By removing the final commits to a separate bash script I was able
     to reduce the total size of the cloud-init to 15,548 bytes.
    
    The separate script requres a parameter which is used in the commit comment.
     This will allow the same script to be used by other cloud-init files.

--- AWS-LAM-git-commit.bash
+++ AWS-LAM-git-commit.bash
@@ -1,40 +1,73 @@
 #!/usr/bin/bash
-#/mnt/efs/aws-lam1-ubuntu/lam1-Ubuntu-ARM-git-commit.bash
-#/var/www/aws/lam1-Ubuntu-ARM-git-commit.bash
+
+#/var/www/aws/AWS-LAM-git-commit.bash
 # This segment was stipped out of /var/www/aws/aws-nwo-lam1-Ubuntu-CloudInit-ARM.txt
-#  because user data is limited to 16384 bytes.
+#  because user data is limited to 16,384 bytes.
 # Removal of this segment and replacement with a call
-#  got me from 17719 to 15535 bytes.
-# This should be rewritten to accept the KEYNAME used in the commit as an argument
+#  got me from 17,719 to 15,531 bytes.
+
+# Program requires the KEYNAME used in the commit as an argument
+
+if [ -z $1 ] ; then # Check that a parameter was provided
+  echo "KEYNAME needed!" && exit 1;
+fi
+
 # It could also accept a list of sites and limit itself to within /var/www or accept
-# that prefix as an argument.
+# that prefix as an argument.  The rest of this could loop to process that list.
 # Thursday, November 2, 2023 @ 10:23:23 PM (Alaska Time) - imlementing IPv6
 
 echo 'AWS LAM commit cloud-init changes in /var/www/aws git repo'
-sh -c "cd /var/www/aws;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/aws
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/no-ssl git repo'
-sh -c "cd /var/www/no-ssl;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/no-ssl
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/arsc git repo'
-sh -c "cd /var/www/arsc;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/arsc
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/sites git repo'
-sh -c "cd /var/www/sites;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/sites
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/cabo git repo'
-sh -c "cd /var/www/cabo;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/cabo
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/z git repo'
-sh -c "cd /var/www/z;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/z
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/blinkenshell/public_html git repo'
-sh -c "cd /var/www/blinkenshell/public_html;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/blinkenshell/public_html
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/olnes git repo'
-sh -c "cd /var/www/olnes;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/olnes
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/alaskademocrat git repo'
-sh -c "cd /var/www/alaskademocrat;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/alaskademocrat
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/larryforalaska git repo'
-sh -c "cd /var/www/larryforalaska;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/larryforalaska
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/lam git repo'
-sh -c "cd /var/www/lam;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/lam
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/mike git repo'
-sh -c "cd /var/www/mike;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/mike
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/interiordems git repo'
-sh -c "cd /var/www/interiordems;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/interiordems
+ git commit -a -m "AWS LAM cloud-init $1"
+
 echo 'AWS LAM commit cloud-init changes in /var/www/oldinteriordems git repo'
-sh -c "cd /var/www/oldinteriordems;git commit -a -m 'AWS LAM cloud-init lam1-Ubuntu-ARM'"
+ cd /var/www/oldinteriordems
+ git commit -a -m "AWS LAM cloud-init $1"
