ead675c76ff27955440fd52d8ac4200a32329290
LAMurakami <GitHub@LAMurakami.com>
2023-11-09 08:56:47 -0700

    Use aws-lam-www-git-clone.bash to clone /var/www/$repo
    The program will handle all but aws blinkenshell and lam git repositories.
    The program is in the aws repo so that repo must be handled first.
    The lam and blinkenshell git repositories are handled separately.
    
    This saved over 1.5 K in the file size in the largest CloudInit file
     getting it further away from the 16K limit for the user data that can be
     supplied with "aws ec2 run-instances" or via the web "Launch instance"
     page.

---
+++ aws-lam-www-git-clone.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+#aws-lam-www-git-clone.bash
+
+if [ -z $2 ] ; then
+  echo "At least two arguments needed!"
+  echo "Usage: $0 <repo> <keyName> [private]"
+  exit 1;
+fi
+
+repo=$1
+keyName=$2
+
+if [ -z $3 ] ; then # default is the public origin (my public github repos)
+  origin1='https://github.com/LAMurakami/'
+  origin2='git@github.com:LAMurakami/'
+else
+  origin1='/mnt/efs/git/'
+  origin2='git@ak20:'
+fi
+
+git clone $origin1$repo /var/www/$repo
+cd /var/www/$repo
+git remote set-url origin $origin2$repo
+[ -z $3 ] &&  git remote add ak20 git@ak20:$repo
+git checkout -b $keyName
