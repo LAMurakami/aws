76a0bd414f60197dddf38d1f7a35ae4fadf40ca9
LAMurakami <GitHub@LAMurakami.com>
2024-02-19 09:58:01 -0900

    Modify repo install to test for REGION
    The git repos are in the us-west-2 Oregon region
    If not in the us-west-2 Oregon region use the ssh to aws connection.
    
    The ssh to aws connection setup requires aws to be in the known_host file.

--- aws-efs-www-git-clone.bash
+++ aws-efs-www-git-clone.bash
@@ -11,7 +11,13 @@ fi
 repo=$1
 keyName=$2
 
+# The git repos are in the us-west-2 Oregon region
+if [ ${REGION} = 'us-west-2' ]; then
+  origin1='/mnt/efs/git/'
+else
+# If not in the us-west-2 Oregon region use the ssh to aws connection
   origin1='aws:/mnt/efs/git/'
+fi
 
 git clone $origin1$repo /var/www/$repo
 cd /var/www/$repo
