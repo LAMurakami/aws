ba6769aa018e127f9bd5adb14dd9d8f1636bf249
LAMurakami <GitHub@LAMurakami.com>
2024-07-25 15:38:41 -0800

    Add target parameter for git clone script

--- aws-lam-gitlab-clone.bash
+++ aws-lam-gitlab-clone.bash
@@ -4,15 +4,21 @@
 
 if [ -z $2 ] ; then
   echo "Two arguments needed!"
-  echo "Usage: $0 <repo> <keyName>"
+  echo "Usage: $0 <repo> <keyName> [target]"
   exit 1;
 fi
 
 REPO=$1
 keyName=$2
 
-git clone https://gitlab.com/aws-lam/${REPO} /var/www/${REPO}
-cd /var/www/${REPO}
+if [ $3 ] ; then
+  target=$3
+else
+  target='/var/www/'
+fi
+
+git clone https://gitlab.com/aws-lam/${REPO} $target${REPO}
+cd $target${REPO}
 git remote set-url origin git@gitlab.com:aws-lam/${REPO}
 git remote add github git@github.com:LAMurakami/${REPO}
 git remote add ak20 git@ak20:${REPO}
