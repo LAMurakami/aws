d4a27f6c4ae8788ded7a477bd3a9ecc733213a90
LAMurakami <GitHub@LAMurakami.com>
2024-08-02 20:12:19 -0800

    Add gitlab remote when cloning from GitHub
    When I first implemented the aws-lam-www-git-clone.bash program I was cloning
    public repos from github over https and then switching the remote to ssh and
    cloning private repos from efs.
    
    Since then I have expanded to being able to launch in 29 AWS regions instead of
    just the us-west-2 Oregon where the /mnt/efs/git/ private copy of my repo
    store is kept.
    
    I also expanded to being able to launch on an instance without a public IPv4
    address where access to GitHub or any store without IPv6 capability is
    unavailble without an IPv6 only workaround.
    
    aws-lam-www-git-clone.bash
    Passing a third parameter specifies private repos will be used
    For private repos
    REGION must be set before use! If not aws ssh connection will be used
    git@localhost must be setup for private repos access in us-west-2 Oregon
    with git user home set to /mnt/efs/git/
    
    I had considered eliminating the use of GitHub when I got the GitLab account
    But then decided to leave it in use for the cloud-init scripts for an
    instance with a Public IPv4 address than can use it.
    I did switch to using the new GitLab script for all but the aws repo.
    
    I have now updated the script so it can be used as an alternate and
    maybe someday GitHub will support IPv6 and it could be an alternate in
    all cases.

--- aws-lam-www-git-clone.bash
+++ aws-lam-www-git-clone.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#aws-lam-www-git-clone.bash
+# aws-lam-www-git-clone.bash
 
 if [ -z $2 ] ; then
   echo "At least two arguments needed!"
@@ -11,17 +11,24 @@ fi
 repo=$1
 keyName=$2
 
-if [ -z $3 ] ; then # default is the public origin (my public github repos)
+if [ -z $3 ] ; then      # default is the public origin (my public github repos)
   origin1='https://github.com/LAMurakami/'
   origin2='git@github.com:LAMurakami/'
-else
-  origin1='/mnt/efs/git/'
+else                          # If private repos will be used REGION must be set
+  if [[ ${REGION} == 'us-west-2' ]]; then
+    origin1='git@localhost:'
+  else                 # If not in the us-west-2 Oregon region or region not set
+    origin1='git@aws:'                 # use ssh to aws connection setup earlier
+  fi
   origin2='git@ak20:'
 fi
 
 git clone $origin1$repo /var/www/$repo
 cd /var/www/$repo
 git remote set-url origin $origin2$repo
-[ -z $3 ] &&  git remote add ak20 git@ak20:$repo
+if [ -z $3 ] ; then     # If public repos Set gitlab and ak20 additional remotes
+  git remote add gitlab git@gitlab.com:aws-lam/$repo
+  git remote add ak20 git@ak20:$repo
+fi
 git checkout -b $keyName
 git tag -a -m "AWS LAM Initialization for $keyName" b0-$keyName
