3a5992cf71cb03fe0b0e06f3c2e143e373dc8072
LAMurakami <GitHub@LAMurakami.com>
2024-08-23 04:04:50 -0800

    Use public aws repo for aws ssh connectivity
    Use public aws repo on GitLab that is avalaible over IPv6 as well as IPv4.
    Use aws-efs-mount.bash program instead of the (AWS) Elastic File System (EFS)
    metadata directory method to mount the AWS LAM VPC EFS unique filesystem for
    the region the instance was launched in.
    
    Omit an unneeded echo

--- aws-efs-mount.bash
+++ aws-efs-mount.bash
@@ -144,8 +144,6 @@ echo "REGION=${REGION} EFS=${EFS}"
 
 nfsOpt="_netdev,noresvport,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0"
 
-echo 'AWS LAM Adding nfs4 mount to AWS NW-O VPC Elastic File System'
-
 mkdir /mnt/efs
 echo "${EFS}:/ /mnt/efs nfs4 ${nfsOpt}" >> /etc/fstab
 mount -a -t nfs4
