1e914081c635d9bd5f419123229a314708fd1172
LAMurakami <GitHub@LAMurakami.com>
2024-12-30 15:39:54 -0900

    Modify Update-DuckDNS.bash for -No-Public-IPv4
    This will allow a -No-Public-IPv4 CloudInit to work for instances with and
     without a Public IPv4 address.
    
    The program now assumes we have a public IPv6 address and a private IPv4
     address which may or may not be associated with a Public IPv4 address.
    
    If the instance does not have a Public IPv4 address it assumes the aws instance
     can be used for the DuckDNS update with ssh connectivity.
    
    It currently ueses the ec2-user-data.pl program which uses Instance Metadata
     Service Version 2 (IMDSv2) session-oriented requests to get multiple value
     pairs and discards all but the public-ipv4 which it tests for the none value.
    
    I did this so the new aws-lam2-openSUSE-No-Public-IPv4-CloudInit.txt could
    be modified to handle an instance with a Puplic IPv4 address and I wouldn't
    have to create yet another CloudInit file.

--- Update-DuckDNS.bash
+++ Update-DuckDNS.bash
@@ -1,18 +1,31 @@
-#!/bin/bash
+#!/usr/bin/bash
+# Update-DuckDNS.bash
 
 if [ -z $1 ] ; then
-  echo "Subdomain for ?.duckdns.org needed!" && exit 1;
+  echo "One argument needed!"
+  echo "Usage: $0 <Subdomain>"
+  exit 1;
 fi
 
-echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
-~/.duckdns)&verbose=true&ip="| curl -s -k -K -; echo \
-" for $1.duckdns.org IP address update" ; echo
+Subdomain=$1
 
 IPv6_Address=$(ip a | grep inet6 \
 | grep 'scope global dynamic' | awk '{print $2;}' \
 | sed 's|/.*||' | tr -d '\n')
 
-echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
-~/.duckdns)&verbose=true&ip=&ipv6=${IPv6_Address}"| curl -s -k -K -; echo \
-" for $1.duckdns.org IP address update" ; echo
+Public_IPv4=$(/var/www/aws/ec2-user-data.pl \
+| grep public-ipv4 | awk "{print \$3;}")
 
+if [[ ${Public_IPv4} == 'none' ]] ; then
+ ssh aws "/var/www/aws/Update-DuckDNS-IPv6-from-IPv4.bash \
+   ${Subdomain} ${IPv6_Address}"
+
+else
+ echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
+ ~/.duckdns)&verbose=true&ip="| curl -s -k -K -; echo \
+ " for $1.duckdns.org IP address update" ; echo
+
+ echo url="https://www.duckdns.org/update?domains=$1&token=$(cat \
+ ~/.duckdns)&verbose=true&ip=&ipv6=${IPv6_Address}"| curl -s -k -K -; echo \
+ " for $1.duckdns.org IP address update" ; echo
+fi
