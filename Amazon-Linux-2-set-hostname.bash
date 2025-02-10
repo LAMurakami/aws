88851bb4cea80f0abfbd63d5fecd05bf62941d23
LAMurakami <GitHub@LAMurakami.com>
2025-02-09 23:17:41 -0900

    Set lam2.lam1.us hostname

--- Amazon-Linux-2-set-hostname.bash
+++ Amazon-Linux-2-set-hostname.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 sh -c "echo 'preserve_hostname: true' >> /etc/cloud/cloud.cfg"
-hostnamectl set-hostname lam2.duckdns.org
-sh -c "echo 'HOSTNAME=lam2.duckdns.org' >> /etc/sysconfig/network"
+hostnamectl set-hostname lam2.lam1.us
+sh -c "echo 'HOSTNAME=lam2.lam1.us' >> /etc/sysconfig/network"
