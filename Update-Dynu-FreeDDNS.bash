35d521ef79cb2e569e86de789777ac35c9baa092
LAMurakami <GitHub@LAMurakami.com>
2025-07-31 22:58:02 -0800

    Use /home/lam/.dynu-password
    Report environment

--- Update-Dynu-FreeDDNS.bash
+++ Update-Dynu-FreeDDNS.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 # Update-Dynu-FreeDDNS.bash
+#
+# Use /home/lam/.dynu-password
 
 if [ -z $1 ] ; then
   echo "One argument needed!"
@@ -9,19 +11,16 @@ fi
 
 Subdomain=$1
 
-# Setup the ~/.dynu-password
-cp /home/lam/.dynu-password .
-
 if [[ ${Public_IPv4} == 'none' ]] ; then
   echo 'Public_IPv4=none'
 else
 
 echo url="https://api-ipv4.dynu.com/nic/update?username=LAMurakami&password=$(cat \
-~/.dynu-password)&hostname=${Subdomain}.freeddns.org"\
+/home/lam/.dynu-password)&hostname=${Subdomain}.freeddns.org"\
 | curl -k -K -; echo " IPv4 address update for ${Subdomain}.freeddns.org" ; echo
 
 fi
 
 echo url="https://api-ipv6.dynu.com/nic/update?username=LAMurakami&password=$(cat \
-~/.dynu-password)&hostname=${Subdomain}.freeddns.org"| curl -k -K -; echo \
+/home/lam/.dynu-password)&hostname=${Subdomain}.freeddns.org"| curl -k -K -; echo \
 " IPv6 address update for ${Subdomain}.freeddns.org" ; echo
